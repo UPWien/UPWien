86a5e2c5634f6f6c057cdf772d4913a15a728620
Eckert <eck18653@spengergasse.at>
2017-03-10 17:56:36 +0100

    lkjhlkj

---
+++ Blumen/src/Blume.java
@@ -0,0 +1,28 @@
+
+public class Blume
+{
+	private double preis;
+	private int anzahl;
+	
+	
+	public double getPreis()
+	{
+		return preis;
+	}
+	
+	public void setPreis(double preis)
+	{
+		this.preis = preis;
+	}
+	
+	public int getAnzahl()
+	{
+		return anzahl;
+	}
+	
+	public void setAnzahl(int anzahl)
+	{
+		this.anzahl = anzahl;
+	}
+	
+}
